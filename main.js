b7a9fa2f4c898e1458cd83b99c4a07f2cc0d8a86
duffaldri <mdaffaaldriantama@gmail.com>
2022-12-07 15:16:58 +0700

    3, 5, 6

--- main.js
+++ main.js
@@ -5,126 +5,54 @@ function main() {
     // Warna canvas-nya
 
     // Koordinat titik-titik
-    var verticesM = [
-        // Huruf M bagian depan | 22 - 35, n = 13
-        -0.95625, -0.45, 0,     1, 0, 1, // Index: 0
-        -0.65625, -0.45, 0,     1, 0, 1, // Index: 1
-        -0.6, 2.4, 0,           1, 0, 1, // Index: 2 Kiri atas
-        -0.45, 1.2, 0,          1, 0, 1, // Index: 3 Tengah kiri
-        -0.3, 2.4, 0,           1, 0, 1, // Index: 4
-        -0.3, 0.0, 0,           1, 0, 1, // Index: 5
-        -0.15, 0.9, 0,          1, 0, 1, // Index 6 Bagian tengah M
-        -0, 0.0, 0,             1, 0, 1, // Index: 7
-        -0, 2.4, 0,             1, 0, 1, // Index: 8
-        0.15, 1.2, 0,           1, 0, 1, // Index: 9 Tengah kanan
-        0.3, 2.4, 0,            1, 0, 1, // Index: 10 Kanan atas
-        0.3, 0, 0,              1, 0, 1, // Index: 11
-        0.525, 0.6, 0,          1, 0, 1, // Index: 12 Kanan bawah, perbatasan dengan A
-        
-        // Huruf M bagian belakang | 22 - 35, n = 13
-        -0.95625, -0.45, -1.0,     0.5, 0, 0.5, // Index: 13
-        -0.65625, -0.45, -1.0,     0.5, 0, 0.5, // Index: 14
-        -0.6, 2.4, -1.0,           0.5, 0, 0.5, // Index: 15 Kiri atas
-        -0.45, 1.2, -1.0,          0.5, 0, 0.5, // Index: 16 Tengah kiri
-        -0.3, 2.4, -1.0,           0.5, 0, 0.5, // Index: 17
-        -0.3, 0.0, -1.0,           0.5, 0, 0.5, // Index: 18
-        -0.15, 0.9, -1.0,          0.5, 0, 0.5, // Index: 19 Bagian tengah M
-        -0, 0.0, -1.0,             0.5, 0, 0.5, // Index: 20
-        -0, 2.4, -1.0,             0.5, 0, 0.5, // Index: 21
-        0.15, 1.2, -1.0,           0.5, 0, 0.5, // Index: 22 Tengah kanan
-        0.3, 2.4, -1.0,            0.5, 0, 0.5, // Index: 23 Kanan atas
-        0.3, 0, -1.0,              0.5, 0, 0.5, // Index: 24
-        0.525, 0.6, -1.0,          0.5, 0, 0.5, // Index: 25 Kanan bawah, perbatasan dengan A
-    ];
-
-    var indicesM = [
-        // Depan
-        0, 1, 2,    1, 2, 3,    2, 3, 4,    3, 4, 5,
-        4, 5, 6,    5, 6, 7,    6, 7, 8,    7, 8, 9,
-        8, 9, 10,   9, 10, 11,  10, 11, 12, 
-        
-        // Belakang
-        13, 14, 15,     14, 15, 16,     15, 16, 17,
-        16, 17, 18,     17, 18, 19,     18, 19, 20,
-        19, 20, 21,     20, 21, 22,     21, 22, 23,
-        22, 23, 24,     23, 24, 25, 
-
-        // Sisi
-        0, 13, 15,
-        0, 2, 15,
-        2, 15, 4,
-        15, 17, 4,
-        4, 17, 19,
-        4, 19, 6,
-        6, 19, 8,
-        19, 8, 21,
-        21, 8, 23,
-        8, 23, 10,
-        23, 10, 12,
-        23, 12, 25,
-        25, 12, 24,
-        12, 24, 11,
-        24, 11, 9,
-        24, 9, 22,
-        22, 9, 20,
-        20, 9, 7,
-        20, 7, 5,
-        20, 18, 5,
-        18, 5, 16,
-        5, 16, 3,
-        16, 3, 1,
-        16, 1, 14,
-        1, 13, 14,
-        0, 1, 13
-    ];
     var verticesA = [
         // Depan
         // Huruf A strip tengah & kaki kanan | 34 - 38. n = 5
-        0.3, 0, 0,              1, 0, 1, // Index: 0
-        0.525, 0.6, 0,          1, 0, 1, // Index: 1
-        1.125, 0, 0,            1, 0, 1, // Index: 2
-        1.35, 0.6, 0,           1, 0, 1, // Index: 3
-        1.18125, -0.45, 0,      1, 0, 1, // Index: 4
-        1.48125, -0.45, 0,      1, 0, 1, // Index: 5
+        1.3, 0, 0,              1, 0, 1, // Index: 0
+        1.525, 0.6, 0,          1, 0, 1, // Index: 1
+        2.125, 0, 0,            1, 0, 1, // Index: 2
+        2.35, 0.6, 0,           1, 0, 1, // Index: 3
+        2.18125, -0.45, 0,      1, 0, 1, // Index: 4
+        2.48125, -0.45, 0,      1, 0, 1, // Index: 5
         
         // Kaki A kaki kiri kiri | 39 - 42, n = 4
-        0.46875, -0.45, 0,      1, 0, 1, // Index: 6
-        0.76875, -0.45, 0,      1, 0, 1, // Index: 7
-        0.51375, -0.09, 0,      1, 0, 1, // Index: 8
-        0.81375, -0.09, 0,      1, 0, 1, // Index: 9
+        1.46875, -0.45, 0,      1, 0, 1, // Index: 6
+        1.76875, -0.45, 0,      1, 0, 1, // Index: 7
+        1.51375, -0.09, 0,      1, 0, 1, // Index: 8
+        1.81375, -0.09, 0,      1, 0, 1, // Index: 9
 
         // Huruf A bagian atas | 43 - 49, n = 7
-        1.33875, 0.69, 0,       1, 0, 1, // Index: 10
-        1.03875, 0.69, 0,       1, 0, 1, // Index: 11
-        1.125, 2.4, 0,          1, 0, 1, // Index: 12
-        0.978, 1.2, 0,          1, 0, 1, // Index: 13
-        0.825, 2.4, 0,          1, 0, 1, // Index: 14
-        0.91125, 0.69, 0,       1, 0, 1, // Index: 15
-        0.60975, 0.69, 0,       1, 0, 1, // Index: 16
+        2.33875, 0.69, 0,       1, 0, 1, // Index: 10
+        2.03875, 0.69, 0,       1, 0, 1, // Index: 11
+        2.125, 2.4, 0,          1, 0, 1, // Index: 12
+        1.978, 1.2, 0,          1, 0, 1, // Index: 13
+        1.825, 2.4, 0,          1, 0, 1, // Index: 14
+        1.91125, 0.69, 0,       1, 0, 1, // Index: 15
+        1.60975, 0.69, 0,       1, 0, 1, // Index: 16
 
         // Belakang
         // Huruf A strip tengah & kaki kanan | 34 - 38. n = 5
-        0.3, 0, -1.0,           0.5, 0, 0.5, // Index: 17
-        0.525, 0.6, -1.0,       0.5, 0, 0.5, // Index: 18
-        1.125, 0, -1.0,         0.5, 0, 0.5, // Index: 19
-        1.35, 0.6, -1.0,        0.5, 0, 0.5, // Index: 20
-        1.18125, -0.45, -1.0,   0.5, 0, 0.5, // Index: 21
-        1.48125, -0.45, -1.0,   0.5, 0, 0.5, // Index: 22
+        1.3, 0, -1.0,           0.5, 0, 0.5, // Index: 17
+        1.525, 0.6, -1.0,       0.5, 0, 0.5, // Index: 18
+        2.125, 0, -1.0,         0.5, 0, 0.5, // Index: 19
+        2.35, 0.6, -1.0,        0.5, 0, 0.5, // Index: 20
+        2.18125, -0.45, -1.0,   0.5, 0, 0.5, // Index: 21
+        2.48125, -0.45, -1.0,   0.5, 0, 0.5, // Index: 22
         
         // Kaki A kaki kiri kiri | 39 - 42, n = 4
-        0.46875, -0.45, -1.0,   0.5, 0, 0.5, // Index: 23
-        0.76875, -0.45, -1.0,   0.5, 0, 0.5, // Index: 24
-        0.51375, -0.09, -1.0,   0.5, 0, 0.5, // Index: 25
-        0.81375, -0.09, -1.0,   0.5, 0, 0.5, // Index: 26
+        1.46875, -0.45, -1.0,   0.5, 0, 0.5, // Index: 23
+        1.76875, -0.45, -1.0,   0.5, 0, 0.5, // Index: 24
+        1.51375, -0.09, -1.0,   0.5, 0, 0.5, // Index: 25
+        1.81375, -0.09, -1.0,   0.5, 0, 0.5, // Index: 26
 
         // Huruf A bagian atas | 43 - 49, n = 7
-        1.33875, 0.69, -1.0,    0.5, 0, 0.5, // Index: 27
-        1.03875, 0.69, -1.0,    0.5, 0, 0.5, // Index: 28
-        1.125, 2.4, -1.0,       0.5, 0, 0.5, // Index: 29
-        0.978, 1.2, -1.0,       0.5, 0, 0.5, // Index: 30
-        0.825, 2.4, -1.0,       0.5, 0, 0.5, // Index: 31
-        0.91125, 0.69, -1.0,    0.5, 0, 0.5, // Index: 32
-        0.60975, 0.69, -1.0,    0.5, 0, 0.5, // Index: 33
+        2.33875, 0.69, -1.0,    0.5, 0, 0.5, // Index: 27
+        2.03875, 0.69, -1.0,    0.5, 0, 0.5, // Index: 28
+        2.125, 2.4, -1.0,       0.5, 0, 0.5, // Index: 29
+        1.978, 1.2, -1.0,       0.5, 0, 0.5, // Index: 30
+        1.825, 2.4, -1.0,       0.5, 0, 0.5, // Index: 31
+        1.91125, 0.69, -1.0,    0.5, 0, 0.5, // Index: 32
+        1.60975, 0.69, -1.0,    0.5, 0, 0.5, // Index: 33
     ];
 
     var indicesA = [
@@ -176,41 +104,6 @@ function main() {
         11, 30, 28,  11, 30, 13,
         30, 15, 13,  30, 15, 32,
 
-    ];
-
-    var vertices1 = [
-        // Depan
-        -1.447500, -0.396000, 0,                1, 0, 1,
-        -1.395000, -0.198000, 0,                1, 0, 1,
-        -1.995000, -0.198000, 0,                1, 0, 1,
-        -1.552500, 1.590000, 0,         1, 0, 1,
-        -1.732500, 1.590000, 0,         1, 0, 1,
-        -1.882500, 1.290000, 0,         1, 0, 1,
-        -1.777500, 1.290000, 0,         1, 0, 1,
-        -2.145000, -0.198000, 0,                1, 0, 1,
-        -2.415000, -0.198000, 0,                1, 0, 1,
-        -2.464500, -0.396000, 0,                1, 0, 1,
-        // Belakang
-        -1.447500, -0.396000, -1,               0.5, 0, 0.5,
-        -1.395000, -0.198000, -1,               0.5, 0, 0.5,
-        -1.995000, -0.198000, -1,               0.5, 0, 0.5,
-        -1.552500, 1.590000, -1,                0.5, 0, 0.5,
-        -1.732500, 1.590000, -1,                0.5, 0, 0.5,
-        -1.882500, 1.290000, -1,                0.5, 0, 0.5,
-        -1.777500, 1.290000, -1,                0.5, 0, 0.5,
-        -2.145000, -0.198000, -1,               0.5, 0, 0.5,
-        -2.415000, -0.198000, -1,               0.5, 0, 0.5,
-        -2.464500, -0.396000, -1,               0.5, 0, 0.5,
-
-    ];
-
-    var indices1 = [
-        // Depan
-        0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 0,
-
-        // Belakang
-        10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 10
-        // Sisi
     ];
 
     var vertices7 = [
@@ -279,25 +172,11 @@ function main() {
 
     var objects = [
         {
-            name: 'M',
-            vertices: verticesM,
-            indices: indicesM,
-            length: 48,
-            type: gl.TRIANGLES,
-        },
-        {
             name: 'A',
             vertices: verticesA,
             indices: indicesA,
             length: 20,
             type: gl.TRIANGLES,
-        },
-        {
-            name: '1',
-            vertices: vertices1,
-            indices: indices1,
-            length: 20,
-            type: gl.LINES,
         },
         {
             name: '7',
@@ -338,9 +217,13 @@ function main() {
     var fragmentShaderCode = `
         precision mediump float;
         varying vec3 vColor;
+        uniform vec3 uAmbientConstant;
+        uniform float uAmbientIntensity;
 
         void main() {
-            gl_FragColor = vec4(vColor, 1.0);
+            vec3 ambient = uAmbientConstant * uAmbientIntensity;
+            vec3 phong = ambient;
+            gl_FragColor = vec4(phong * vColor, 1.0);
         }
     `;
     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
@@ -359,10 +242,6 @@ function main() {
     var moveZ = 0.0;
     var moveX = 0.0;
     var freeze = false;
-    var horizontalSpeed = 0.0177;
-    var canvasWidth = 9;
-    var horizontalDelta = 0.0;
-    var verticalDelta = 0.0;
     var scaleSpeed = 0.05;
     var scaleDelta = 0;
 
@@ -414,28 +293,10 @@ function main() {
         gl.drawElements(glType, indices.length, gl.UNSIGNED_SHORT, 0);
     }
 
-    const translate1 = () =>{
-        var model = mat4.create();
-    
-        if (horizontalDelta >= (canvasWidth/2+2) || horizontalDelta <= (-canvasWidth/2+2)) {
-          horizontalSpeed = horizontalSpeed * -1;
-        }
-    
-        horizontalDelta += horizontalSpeed;
-    
-        mat4.translate(model, model, [horizontalDelta, verticalDelta, 0.0]);
-        
-        var uModel = gl.getUniformLocation(shaderProgram, "uModel");
-        var uView = gl.getUniformLocation(shaderProgram, "uView");
-        var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
-    
-        gl.uniformMatrix4fv(uModel,false, model);
-        gl.uniformMatrix4fv(uView, false, view);
-        gl.uniformMatrix4fv(uProjection, false, perspective);
-    
-        draw(objects[2].vertices, objects[2].indices, 0, objects[2].length, objects[2].type);
-      }
-    
+    var uAmbientConstant = gl.getUniformLocation(shaderProgram, "uAmbientConstant");
+    var uAmbientIntensity = gl.getUniformLocation(shaderProgram, "uAmbientIntensity");
+    gl.uniform3fv(uAmbientConstant, [1.0, 1.0, 1.0]);
+    gl.uniform1f(uAmbientIntensity, 0.177);
 
     function onKeydown(event) {
         if (event.keyCode == 37) { // Kiri
@@ -507,26 +368,8 @@ function main() {
         gl.uniformMatrix4fv(uView, false, view);
         gl.uniformMatrix4fv(uProjection, false, perspective);
 
-        draw(objects[1].vertices, objects[1].indices, 0, objects[1].length, objects[1].type);
+        draw(objects[0].vertices, objects[0].indices, 0, objects[0].length, objects[0].type);
         
-        // Translation tapi gagal :(
-        // var model1 = mat4.create(); // Membuat matriks identitas
-        
-        // if((horizontalDelta >= canvasWidth/2) || (horizontalDelta <= (canvasWidth/2)+1))
-        //     horizontalSpeed = horizontalSpeed * -1;
-        
-        // horizontalDelta += horizontalSpeed;
-        // mat4.translate(
-        //     model1, model1, [horizontalDelta, verticalDelta, 0.0]
-        // );
-
-        // gl.uniformMatrix4fv(uModel, false, model1);
-        // gl.uniformMatrix4fv(uView, false, view);
-        // gl.uniformMatrix4fv(uProjection, false, perspective);
-
-        // draw(objects[2].vertices, objects[2].indices, 0, objects[2].length, objects[2].type);
-        
-        // Angka 7 Scaling
         var model7 = mat4.create(); 
 
         if (scaleDelta >= 2 || scaleDelta <= -0.5) {
@@ -541,10 +384,8 @@ function main() {
         gl.uniformMatrix4fv(uView, false, view);
         gl.uniformMatrix4fv(uProjection, false, perspective);
 
-        draw(objects[3].vertices, objects[3].indices, 0, objects[3].length, objects[3].type);
+        draw(objects[1].vertices, objects[1].indices, 0, objects[1].length, objects[1].type);
         
-        translate1();
-        // scale7();
 
         // Cube
         var modelCube = mat4.create(); 
@@ -555,7 +396,7 @@ function main() {
         gl.uniformMatrix4fv(uView, false, view);
         gl.uniformMatrix4fv(uProjection, false, perspective);
 
-        draw(objects[4].vertices, objects[4].indices, 0, objects[4].length, objects[4].type);
+        draw(objects[2].vertices, objects[2].indices, 0, objects[2].length, objects[2].type);
 
         requestAnimationFrame(render);
     }
